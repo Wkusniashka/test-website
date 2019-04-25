26367ff7c3f7a9942c3766673927715541555c34
Anastasiia Pokropyvna <anastasiya.pokropyvna@gmail.com>
2019-04-25 12:56:14 +0100

    g_carousel

---
+++ index.js
@@ -0,0 +1,59 @@
+let slideShow;
+let slideIndex = 0;
+
+document.addEventListener("keydown", function(event) { keyDownHandler(event) });
+
+function keyDownHandler(event) {
+  switch (event.keyCode) {
+    case 39:
+      nextSlide();
+      event.preventDefault();
+      break;
+    case 37:
+      prevSlide();
+      event.preventDefault();
+      break;
+  }
+}
+
+function changeSlide(index) {
+  let slides = document.getElementsByClassName("slideItem");
+  let img_texts = document.getElementsByClassName("img_desc");
+  if (index >= slides.length - 1) {slideIndex = 0}
+  if (index <= 0) {slideIndex = slides.length - 1}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    img_texts[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+  img_texts[slideIndex].style.display = "inline-block";
+}
+
+function prevSlide() {
+  slideIndex--;
+  changeSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  changeSlide(slideIndex);
+}
+
+function autoNextSlide() {
+  nextSlide();
+}
+
+function stopSlideShow() {
+  clearTimeout(slideShow);
+}
+
+function startSlideShow() {
+  stopSlideShow(slideShow);
+  slideShow = setInterval(autoNextSlide, 3000);
+}
+
+changeSlide();
+
+startSlideShow();
+
+
